d9c45a1d68a880de92cab67d5757a89670703274
MarchenkoIY <khr3b96@gmail.com>
2022-06-29 21:54:45 +0300

    Homework 6

--- Lesson8/main.js
+++ Lesson8/main.js
@@ -1,40 +1,51 @@
-function Cat(name) {
+function Animal() {
     var foodAmount = 50;
-
-    function formatFoodAmount() {
-        return foodAmount + ' гр.';
-    }
-
-    this.dailyNorm = function(amount) {
-        if (!arguments.length) return formatFoodAmount();
-
-        if (amount < 30 || amount > 100) {
-            return 'Недопустимое количество корма.';
-        }
-
-        foodAmount = amount;
-    };
-
-    this.name = name;
-
-    this.stroke = function() {
+  
+      function formatFoodAmount() {
+          return foodAmount + ' гр.';
+      }
+  
+      this.dailyNorm = function(amount) {
+          if (!arguments.length) return formatFoodAmount();
+  
+          if (amount < 30 || amount > 100) {
+              return 'Недопустимое количество корма.';
+          }
+  
+          foodAmount = amount;
+      };
+  
+      this.name = name;
+  
+      this.feed = function() {
+          console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+          return this;
+      };
+  }
+  
+  function Cat(name) {
+      Animal.call(this);
+      
+      var catFeed = this.feed;
+      this.feed = function () {
+        catFeed.call(this)
+        console.log('Кот доволен ^_^');
+        return this;
+      };
+    
+      this.stroke = function () {
         console.log('Гладим кота.');
         return this;
-    }
-
-    this.feed = function() {
-        console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
-        return this;
-    };
-}
-
-var barsik = new Cat('Барсик');
-
-console.log(barsik.dailyNorm());
-barsik.feed();
-
-console.log(barsik.dailyNorm(200));
-barsik.feed().stroke().feed().stroke().feed();
-
-console.log(barsik.dailyNorm(75));
-barsik.feed();
+      };
+  }
+  
+  var barsik = new Cat('Барсик');
+  
+  console.log(barsik.dailyNorm());
+  barsik.feed();
+  
+  console.log(barsik.dailyNorm(200));
+  barsik.feed();
+  
+  console.log(barsik.dailyNorm(75));
+  barsik.stroke().feed().stroke().stroke().feed();
